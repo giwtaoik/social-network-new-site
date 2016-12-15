60635288180834f6f265dde82a4844de5252ef1a
zodiac066 <zodiac066@live.com>
2016-12-15 15:26:24 +0200

    Add files via upload

---
+++ connectionDB.php
@@ -0,0 +1,11 @@
+<?php
+$con=mysql_connect("sql212.gr.tn","grtn_19218641","ellada123");
+
+if(!$con){
+	die('Could not Connect to mysql:'.mysql_error());
+}
+
+mysql_set_charset("utf8",$con);
+mysql_selectdb("grtn_19218641_skybox",$con);
+
+?>
