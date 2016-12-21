7de600bf1bdf7bc8c19bc3760cfb50d70f79e3c1
giwtaoik <giwta_oik@hotmail.com>
2016-12-21 11:22:28 +0200

    Add files via upload

---
+++ LogIn.php
@@ -0,0 +1,42 @@
+<?php
+header("Expires: Sat, 01 Jan 3099 00:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+header("Cache-Control: post-check=0, pre-check=0",false);
+session_cache_limiter("must-revalidate");
+
+session_start();
+$access=false;
+
+require'connectionDB.php';
+	
+//elegxos ama o xristis patise eisodo i dimiourgia xristi
+if ( $_POST["submission"] == "LogIn")
+{
+	$email=$_POST["email"];
+	$pass=$_POST["password"];
+	
+    //elegxos stin vasi ama uparxi o xristis me ta dedomena pou eisigage
+	$result=mysql_query("SELECT * FROM users WHERE Email='$email' AND password='$pass'");
+	$row=mysql_fetch_array($result);
+	
+	if($row)
+		$access=true;
+    
+    //ama uparxi apothikeusi tou AA tou apo ton pinaka users kai metafora tou stin kentriki selida
+	if($access==true)
+	{
+		$_SESSION['log']=$row['AA'];
+		include ("main.php");
+	}
+    //ama den uparxi emfanisi minimatos sfalmatos
+	else
+	{
+		echo file_get_contents("LogIn.html");
+		echo "<p style='margin-top: -130px;'><CENTER><font color='red'> Λάθος Email ή Κωδικός! </font></CENTER></p>";
+	}
+}
+// ama o xristis patise na kanei dimiourgia logariasmou metafora tou stin katallili selida
+else
+	echo file_get_contents("SignUp.html");
+
+?>
