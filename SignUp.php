b7e7092ecde3f9395f8a689c7c81ce4f6efdd85b
giwtaoik <giwta_oik@hotmail.com>
2017-01-07 19:37:55 +0200

    Add files via upload

---
+++ SignUp.php
@@ -0,0 +1,78 @@
+<?php
+
+require'connectionDB.php';
+	
+$flag = false;
+
+//ekxwrisi twn dedomenwn pou eisagage o xristis stin forma eggrafis se metavlites me tin methodo post
+$name = $_POST["name"];
+$lastname = $_POST["lastname"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$password2 = $_POST["password2"];
+
+//elegxos ama o kwdikos tairiazei me tin epanalupsi tou kwdikou
+if($password == $password2)
+	$flag = true;
+else
+{
+	$flag = false;
+	echo file_get_contents("SignUp.html");
+	echo "<p style='margin-top: -170px;'><CENTER><font color='red'> Passwords do not match! </font></CENTER></p>";
+}
+
+
+//elegxos ama to email pou eisigage o xristis exei tin morfi tou email
+if($flag == true)
+{
+    if(eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email))  
+        $flag=true;
+    else
+    {
+        $flag=false;
+        echo file_get_contents("SignUp.html");
+    	echo "<p style='margin-top: -170px;'><CENTER><font color='red'> Incorect Email address! </font></CENTER></p>";
+    }   
+}
+	
+//elegxos ama to email pou eisagage o xristis uparxi idi stin vasi
+if($flag == true)
+{
+    $result=mysql_query("SELECT * FROM users WHERE Email='$email'");
+    $row=mysql_fetch_array($result);
+	
+	if($row)
+		$access=true;
+    
+    if($access==true)
+    {
+        $flag = false;
+        echo file_get_contents("SignUp.html");
+		echo "<p style='margin-top: -170px;'><CENTER><font color='red'> The email you entered already exists! </font></CENTER></p>";
+    }
+ 
+}
+
+//elegxos ama o kwdikos einai panw apo 6 xaraktires
+if($flag == true)
+{
+    $length=strlen($password);
+    if($length<6)
+    {
+        $flag = false;
+        echo file_get_contents("SignUp.html");
+    	echo "<p style='margin-top: -170px;'><CENTER><font color='red'> The password must be at least 6 characters long </font></CENTER></p>";
+    }
+}
+
+//ama oi elegxoi perastikan me epituxia eggrafi tou xristi stin vasi kai metafora tou stin selida tou login na kanei eisodo
+if($flag == true)
+{
+	mysql_query("INSERT INTO users (Name,LastName,Email,password)
+			VALUES('$name','$lastname','$email','$password')");
+			
+	echo file_get_contents("LogIn.html");
+	echo "<p style='margin-top: -405px;'><CENTER><font color='red'> Success! </font></CENTER></p>";
+}
+	
+?>
---
+++ download.php
@@ -0,0 +1,32 @@
+
+<?php
+
+require'connectionDB.php';
+
+
+//php code to download the clicked file from the table
+if(isset($_GET['AA']))
+{
+// if id is set then get the file with the id from database
+$AA    = $_GET['AA'];
+$query = "SELECT Name, Type, Size, Content " .
+         "FROM files WHERE AA = '$AA'";
+         
+//apothikeusi twn stoixeiwn tou arxeiou se metavlites         
+ $result = mysql_query($query);
+$row = mysql_fetch_array($result);
+$size = $row['Size'];
+$type = $row['Type'];
+$name =$row['Name'];
+$fileContent = $row['Content'];
+
+//emfanisei parathurou ston xristi me ta stoixeia tou arxeiou kai to periexomeno tou gia katevasma
+header("Content-length: $size");
+header("Content-type: $type");
+header("Content-Disposition: attachment; filename=$name");
+echo $fileContent;
+
+exit;
+}
+
+?>
